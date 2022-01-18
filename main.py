d1ae420906b39afc9d2f8e89d5bba53c30aa660d
Vineet Kumar <vineet.kumar_cs20@gla.ac.in>
2022-01-18 11:08:01 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+#USE_PYTHON_AS_CALCULATOR
+5+6
+#add
+print(5+6)
+
+#multiply
+print (68*99663)
+
+#multiply
+print (68**99663)
+
+#equation
+print ((89+78-65)*65)
+
+#divisionwithout.value
+print (78//5)
+
+#division
+print (78/5)
+
+#power
+print (5**2)
+
+#power
+print (2**10)
+
+#equation
+print ((45+90-5/8)+(45+23-2/7*89))
+
+#power
+print (4478562166248**535)
